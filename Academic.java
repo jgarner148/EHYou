faa16a343918e2134bbd3d6737620630bf21ca27
Jack Garner <77286854+jgarner148@users.noreply.github.com>
2021-10-22 10:19:42 +0100

    Commenting the class methods

--- Academic.java
+++ Academic.java
@@ -2,15 +2,18 @@
 public class Academic extends Tutor{
 	private Research[] CurrentResearch;
 
+	//Constructor
 	public Academic(String fname, String lname, String dob, int staffNum, String office, Research[] currentResearch) {
 		super(fname, lname, dob, staffNum, office);
 		this.CurrentResearch = currentResearch;
 	}
-
+	
+	//Getter for the research object array
 	public Research[] getCurrentResearch() {
 		return CurrentResearch;
 	}
 
+	//Method to add a new research object to the research object array
 	public void addToCurrentResearch(Research currentResearch) {
 		this.CurrentResearch = AddToArray.research(this.CurrentResearch, currentResearch);
 	}
