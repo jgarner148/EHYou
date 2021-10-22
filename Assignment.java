c94f6a4da6846fc2fa989288710cd972b48c9957
Jack Garner <77286854+jgarner148@users.noreply.github.com>
2021-10-22 10:27:54 +0100

    Adding more comments

--- Assignment.java
+++ Assignment.java
@@ -15,32 +15,38 @@ public class Assignment{
 	}
 	
 
+	//Getter for the moddule Module object
 	public Module getModulefrom() {
 		return modulefrom;
 	}
 
-
+	//Setter for the module from Module object
 	public void setModulefrom(Module modulefrom) {
 		this.modulefrom = modulefrom;
 	}
 
 
+	//Getter for the issue week variable
 	public int getIssueweek() {
 		return issueweek;
 	}
-
+	
+	//Setter for the Issue week variable
 	public void setIssueweek(int issueweek) {
 		this.issueweek = issueweek;
 	}
 
+	//Getter for the grade variable
 	public int getGrade() {
 		return grade;
 	}
 
+	//Stter for the grade variable
 	public void setGrade(int grade) {
 		this.grade = grade;
 	}
-
+	
+	//Getter for the feedback variable
 	public String getFeedback() {
 		return feedback;
 	}
