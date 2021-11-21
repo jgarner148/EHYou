b9ec45f74b1ad74e48b068fa0500fd5eaaba4fe4
Jack Garner <77286854+jgarner148@users.noreply.github.com>
2021-11-21 20:57:32 +0000

    Create GUIWelcomePage.java

---
+++ GUIWelcomePage.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUIWelcomePage implements ActionListener {
+    private JFrame mainFrame = new JFrame();
+    private JPanel welcomePanel = new JPanel();
+
+    public GUIWelcomePage(JFrame mainFrame) {
+        this.mainFrame = mainFrame;
+
+//https://www.youtube.com/watch?v=Hiv3gwJC5kw&t=1420s
+
+
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
