9d1167ee86b90f7de88b7166931adef094bcd336
Jack Garner <77286854+jgarner148@users.noreply.github.com>
2021-11-25 11:31:26 +0000

    Builidng view student

--- GUIViewStudent.java
+++ GUIViewStudent.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
-public class GUIViewStudent {
+public class GUIViewStudent implements ActionListener {
     private JFrame IDCardFrame = new JFrame();
     private String studentCode;
     private JLabel IDLabel = new JLabel("");
@@ -13,12 +15,14 @@ public class GUIViewStudent {
     private JLabel endYrLabel = new JLabel("");
     private JLabel allModulesLabel = new JLabel("");
     private JLabel allResultsLabel = new JLabel("");
+    private JLabel containerLabel = new JLabel(" ");
+    private JButton calculateButton = factory.makeFlatButton("Work out average per module");
 
 
     public GUIViewStudent( String studentCode) throws IOException, ClassNotFoundException {
         this.studentCode = studentCode;
         Student studentBeingViewed = getobject.student(this.studentCode);
-        this.IDCardFrame.setSize(480,480);
+        this.IDCardFrame.setSize(560,560);
         this.IDCardFrame.getContentPane().setBackground(new Color(165,213,220));
         this.IDCardFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE );
         this.IDCardFrame.setTitle("ID card - " + studentBeingViewed.getFname() + " " + studentBeingViewed.getLname() + " (" + studentBeingViewed.getStudentNum() + ")");
@@ -29,24 +33,31 @@ public class GUIViewStudent {
 
         int xLabel = 20;
         int wLabel = 250;
-        int hLabel = 15;
+        int hLabel = 25;
+        Font labelFont = new Font("Georgia", Font.ITALIC,15);
 
         IDLabel.setBounds(xLabel,50,wLabel,hLabel);
+        IDLabel.setFont(labelFont);
         IDLabel.setText("Student Number: " + studentBeingViewed.getStudentNum());
 
         fnameLabel.setBounds(xLabel,70,wLabel,hLabel);
+        fnameLabel.setFont(labelFont);
         fnameLabel.setText("First Name: " + studentBeingViewed.getFname());
 
         lnameLabel.setBounds(xLabel,90,wLabel,hLabel);
+        lnameLabel.setFont(labelFont);
         lnameLabel.setText("Last name: " + studentBeingViewed.getLname());
 
         dobLabel.setBounds(xLabel,110,wLabel,hLabel);
+        dobLabel.setFont(labelFont);
         dobLabel.setText("Date of Birth: " + studentBeingViewed.getDob());
 
         staryYrLabel.setBounds(xLabel,130,wLabel,hLabel);
+        staryYrLabel.setFont(labelFont);
         staryYrLabel.setText("Year Started: " + studentBeingViewed.getStartYr());
 
         endYrLabel.setBounds(xLabel,150,wLabel,hLabel);
+        endYrLabel.setFont(labelFont);
         endYrLabel.setText("Last Year: " + studentBeingViewed.getEndYr());
 
         String[] allModules = studentBeingViewed.getModulesTaking();
@@ -55,14 +66,30 @@ public class GUIViewStudent {
             String currentcode = allModules[i];
             System.out.println(currentcode);
             Module currentModule = getobject.module(currentcode);
-            allModulesText = allModulesText + currentModule.getModName() + " - " + currentModule.getModCode() + " \n";
+            allModulesText = allModulesText + currentModule.getModName() + " - " + currentModule.getModCode() + "<br>";
         }
 
-        allModulesLabel.setBounds(xLabel, 200, wLabel, 240);
-        allModulesLabel.setText(allModulesText);
+        allModulesLabel.setBounds(xLabel, 100, wLabel, 240);
+        allModulesLabel.setFont(labelFont);
+        allModulesLabel.setText("<html>" + "Modules:<br>" + allModulesText);
 
-        allResultsLabel.setBounds(120, 200, wLabel, 240);
-        allResultsLabel.setText("this is a test");
+        String[] allResults = studentBeingViewed.getAllResults();
+        String AllResultsText = "";
+        for(int i = 0; i<allResults.length; i++){
+            String currentcode = allResults[i];
+            System.out.println(currentcode);
+            Result currentResult = getobject.result(currentcode);
+            AllResultsText = AllResultsText + currentResult.getResultCode() + " - " + currentResult.getGrade() + "<br>";
+        }
+
+        allResultsLabel.setBounds(220, 100, wLabel, 240);
+        allResultsLabel.setFont(labelFont);
+        allResultsLabel.setText("<html>" + "Results:<br>" + AllResultsText + "test");
+
+        calculateButton.setBounds(xLabel, 425,200, 50);
+        calculateButton.addActionListener(this);
+
+        containerLabel.setText("hi");
 
 
         IDCardFrame.add(IDLabel);
@@ -73,7 +100,26 @@ public class GUIViewStudent {
         IDCardFrame.add(endYrLabel);
         IDCardFrame.add(allModulesLabel);
         IDCardFrame.add(allResultsLabel);
+        IDCardFrame.add(calculateButton);
+        IDCardFrame.add(containerLabel);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==calculateButton){
+            System.out.println("Button worked");
+            JFrame calulateFrame = new JFrame();
+            calulateFrame.setSize(240,240);
+            calulateFrame.getContentPane().setBackground(new Color(165,213,220));
+            calulateFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            calulateFrame.setTitle("Calculate");
+            ImageIcon logoIcon = new ImageIcon("Images/icon.png");
+            calulateFrame.setIconImage(logoIcon.getImage());
+            calulateFrame.setResizable(false);
+            calulateFrame.setVisible(true);
 
 
+        }
     }
 }
